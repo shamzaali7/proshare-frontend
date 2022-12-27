e7966a6d0e9b74b6961b55d586fb20ace0978249
Hamzaali7 <Hamzaali7@yahoo.com>
2022-12-27 17:51:19 -0500

    Firebase install and files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import React from 'react'
 
 function App() {
+  
+
   return (
     <div className="App">
+      <button onClick={handleGithubLogin}>Login with Github</button>
     </div>
   );
 }
---
+++ src/Config/firebase-config.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD-MdXG7MGe3a3CtlItZ2xFa4vuiL8OLFY",
+    authDomain: "proshare-f25cd.firebaseapp.com",
+    projectId: "proshare-f25cd",
+    storageBucket: "proshare-f25cd.appspot.com",
+    messagingSenderId: "641029913690",
+    appId: "1:641029913690:web:10ba2a71928a1dbd146c63",
+    measurementId: "G-0NGS2C229G"
+  };
+  
+  // Initialize Firebase
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { auth, db };
