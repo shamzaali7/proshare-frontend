329f343774556bc75f20fe4569a2c7dfd734692a
Hamzaali7 <Hamzaali7@yahoo.com>
2023-01-03 13:10:27 -0500

    Home page get route display

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,8 +1,41 @@
-import React from 'react'
+import axios from 'axios';
+import {useState, React, useEffect} from 'react'
 
 function Home(){
+    const [projects, setProjects] = useState([])
+    const axiosProjects = {
+        method: "GET",
+        url: 'https://proshare-backend.herokuapp.com/api/projects'
+    }
+
+    useEffect(() => {
+        getProjects()
+      }, []);
+
+    function getProjects(){
+        axios.request(axiosProjects)
+            .then(function(res){
+                setProjects(res.data)
+            })
+    }
     return(
-        <div>
+        <div className="container-home">
+            {projects.map((project) => {
+                return(
+                    <div className="home-projects">
+                        <div>Title: {project.title}</div>
+                        <div>Deployed Link: <a href={project.deployedLink} target="_blank">{project.deployedLink}</a></div>
+                        <div>Repo: <a href={project.github} target="_blank">{project.github}</a></div>
+                        <div>Showcase:</div>
+                        <div className="container-showcase"> <div></div><div className="showcase"><img src={project.picture}/></div><div></div></div>
+                        <div>Comments: {project.comments.map((comment)=> {
+                            return(
+                                <div>{comment}</div>
+                            )
+                        })}</div>
+                    </div>
+                )
+            })}
         </div>
     )
 }
