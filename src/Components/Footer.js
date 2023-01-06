2f68ee166e775cebeb4b310ab9a8ed7e6bacdb08
Hamzaali7 <Hamzaali7@yahoo.com>
2023-01-06 04:34:50 -0500

    Footer content

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -7,7 +7,7 @@ function Footer(){
     return (
         <div className="container-footer">
             <div className="box-links">
-                <div className="creator"><span className="sp-creator">Creator:</span> Hamza Ali</div>
+                <div className="creator"><span className="sp-creator">Developed by:</span> Hamza Ali</div>
                 <div className="links"><a href="https://github.com/shamzaali7" target="_blank" rel="noreferrer"><BsGithub/></a></div>
                 <div className="links"><a href="https://www.linkedin.com/in/hamza-ali7/" target="_blank" rel="noreferrer">< AiOutlineLinkedin/></a></div>
             </div>
